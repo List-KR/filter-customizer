2afb02e05b1d1faca4db49b8b72005f308c39e2c
PiQuark6046 <piquark6046@proton.me>
2023-07-07 00:36:22 +0000

    Cleanup json-init.ts

--- lib/json-init.ts
+++ lib/json-init.ts
@@ -5,14 +5,16 @@ import * as Errors from './errors.js'
 import * as Got from 'got'
 import * as JSONInterface from './JSONInterface.js'
 
-
 export function Init() {
+  // Config.json
   // Load JSON file.
   try { JSON.parse(Fs.readFileSync('../config.json', 'utf8')) } catch { throw new Errors.JSONInvaildError('The config.json is not a JSON format.') }
   let ConfigJSON = JSON.parse(Fs.readFileSync('../config.json', 'utf8'))
-  
   // Check if JSON is valid.
-  if (!(Superstruct.is(ConfigJSON, ConfigJSONFormat) || Superstruct.is(ConfigJSON, Superstruct.array(ConfigJSONFormat)))) {
+  if (!(Superstruct.is(ConfigJSON, JSONInterface.ConfigJSONFormat) || Superstruct.is(ConfigJSON, Superstruct.array(JSONInterface.ConfigJSONFormat)))) {
     throw new Errors.JSONFormatError('The config.json does not fit a format used in filter-customizer.')
   }
+
+  // predefined-filters.json
+  
 }
